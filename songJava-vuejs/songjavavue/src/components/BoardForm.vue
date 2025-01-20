e494e6067644609005281a6e2226309f3db209a1
Silver-Eun <h22sang@naver.com>
2025-01-20 19:19:20 +0900

    songJava - 4
    file upload

--- songJava-vuejs/songjavavue/src/components/BoardForm.vue
+++ songJava-vuejs/songjavavue/src/components/BoardForm.vue
@@ -2,15 +2,19 @@
   <div class="container">
     <div class="mb-3">
       <label for="title" class="form-label">제목</label>
-      <input type="text" name="title" class="form-control" id="title" placeholder="입력해주세요">
+      <input type="text" v-model="data.board.title" name="title" class="form-control" id="title" placeholder="입력해주세요">
     </div>
     <div class="mb-3">
       <label for="contents" class="form-label">내용</label>
-      <textarea name="contents" class="form-control" id="contens" rows="3"></textarea>
+      <textarea name="contents" v-model="data.board.contents" class="form-control" id="contens" rows="3"></textarea>
     </div>
     <div class="mb-3">
-      <label for="uploadFile" class="form-label"></label>
-      <input name="uploadFile" class="form-control" type="file" id="uploadFile">
+      <label for="uploadFile" class="form-label">파일첨부</label>
+      <input name="uploadFile" v-on:change="changeFile" class="form-control" type="file" id="uploadFile">
+    </div>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <button class="btn btn-primary me-md-2" type="button">취소</button>
+      <button class="btn btn-primary" type="button" v-on:click="save">저장</button>
     </div>
   </div>
 </template>
@@ -24,41 +28,51 @@ export default {
   },
   setup() {
     const data = reactive({
-      boardList: []
+      board: {
+        boardType: 'NOTICE',
+        title: null,
+        contents: null,
+        uploadFile: null
+      }
     });
     // API 호출
     const save = () => {
-      fetch('http://localhost:9000/community')
+      var formData = new FormData();
+      formData.append('boardType', data.board.boardType);
+      if (data.board.title != null) {
+        formData.append('title', data.board.title);
+      }
+      if (data.board.contents != null) {
+        formData.append('contents', data.board.contents);
+      }
+      if (data.board.uploadFile != null) {
+        formData.append('uploadFile', data.board.uploadFile);
+      }
+      fetch('http://localhost:9000/board/save', {
+        method: 'PUT',
+        body: formData
+      })
         .then(response => response.json())
         .then(response => {
-          data.boardList = response.data;
+          console.log(response);
         });
     };
+    const changeFile = (e) => {
+      data.board.uploadFile = e.target.files[0];
+      console.log(data.board.uploadFile);
+    };
     onMounted(() => {
-      save();
-    })
+      
+    });
     return {
       data: data,
-      getList: save
+      save: save,
+      changeFile : changeFile
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+@import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css';
 </style>
