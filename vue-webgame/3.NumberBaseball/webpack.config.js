52ff7286f24d9a310ff105480b886c6364317482
Silver-Eun <h22sang@naver.com>
2025-02-01 22:50:17 +0900

    zerocho - 11
    use webpack-loader

--- vue-webgame/3.NumberBaseball/webpack.config.js
+++ vue-webgame/3.NumberBaseball/webpack.config.js
@@ -1,13 +1,24 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.vue']
+    },
     entry: {
         app: path.join(__dirname, 'main.js'),
     },
     module: {
-        rules: [{}]
+        rules: [{
+            test: /\.vue$/,
+            use: 'vue-loader',
+        }]
     },
-    plugins: [],
+    plugins: [
+        new VueLoaderPlugin(),
+    ],
     output: {
         filename: 'app.js',
         path: path.join(__dirname, 'dist'),
