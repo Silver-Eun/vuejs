15244be4a04a05706690a49ec86bda937ba84c62
Silver-Eun <h22sang@naver.com>
2025-03-06 23:35:36 +0900

    zerocho - 14
    webpack watch

---
+++ vue-webgame/4.responseCheck/responseCheck.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div id="screen">{{}}</div>
+    <div>
+      <div>Avg Time: {{}}</div>
+      <button @click="onReset">Reset</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      result: [],
+    }
+  },
+  methods: {
+    onReset() {
+
+    }
+  }
+}
+</script>
+
+<style></style>
