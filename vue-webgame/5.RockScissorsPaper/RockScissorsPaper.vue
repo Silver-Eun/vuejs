c44d08d325a77ec868b898e14b802977da24dca8
Silver-Eun <h22sang@naver.com>
2025-03-26 22:32:45 +0900

    zerocho - 20
    :class, :style

--- vue-webgame/5.RockScissorsPaper/RockScissorsPaper.vue
+++ vue-webgame/5.RockScissorsPaper/RockScissorsPaper.vue
@@ -1,16 +1,43 @@
 <template>
   <div>
-
+    <div id="computer" :style="computedStyleObject"></div>
+    <div>
+      <button @click="onClickButton('rock')">Rock</button>
+      <button @click="onClickButton('scissors')">Scissors</button>
+      <button @click="onClickButton('paper')">Paper</button>
+    </div>
+    <div>{{ result }}</div>
+    <div>score: {{ score }}</div>
   </div>
 </template>
 
 <script>
+const rspCoords = {
+  rock: '0',
+  scissors: '-142px',
+  paper: '-284px',
+}
+
 export default {
   data() {
-    return {}
+    return {
+      imgCoord: rspCoords.rock,
+      result: '',
+      score: 0,
+    }
   },
-  computed: {},
-  methods: {}
+  computed: {
+    computedStyleObject() {
+      return {
+        background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`,
+      };
+    }
+  },
+  methods: {
+    onClickButton(choice) {
+
+    }
+  }
 }
 </script>
 
