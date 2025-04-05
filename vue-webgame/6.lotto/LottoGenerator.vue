f45653da253270a6aecbb10e22c8ba5e8b3d7891
Silver-Eun <h22sang@naver.com>
2025-04-05 19:20:01 +0900

    zerocho - 24
    child component and props

--- vue-webgame/6.lotto/LottoGenerator.vue
+++ vue-webgame/6.lotto/LottoGenerator.vue
@@ -2,7 +2,7 @@
   <div>
     <div>당첨 숫자</div>
     <div id="result">
-      <lotto-ball v-for="ball in winBalls"></lotto-ball>
+      <lotto-ball v-for="ball in winBalls" number="10"></lotto-ball>
     </div>
     <div>보너스</div>
     <lotto-ball v-if="bonus"></lotto-ball>
@@ -13,11 +13,23 @@
 <script>
 import LottoBall from "./LottoBall.vue";
 
+function getWinNumbers() {
+  const candidate = Array(45).fill().map((v, i) => i + 1);
+  const shuffle = [];
+  while (candidate.length > 0) {
+    shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0]);
+  }
+  const bonusNumber = shuffle[shuffle.length - 1];
+  const winNumbers = shuffle.slice(0, 6).sort((p, c) => p - c);
+  return [...winNumbers, bonusNumber];
+}
+
 export default {
-  components: {LottoBall},
+  components: {'lotto-ball': LottoBall},
   data() {
     return {
-      winBalls: {},
+      winNumbers: getWinNumbers(),
+      winBalls: [],
       bonus: null,
       redo: false,
     }
