a51188b63d6ce295771b839fed16ca144194a687
Silver-Eun <h22sang@naver.com>
2025-04-05 19:06:35 +0900

    zerocho - 23
    LottoGenerator(vue style guide)

---
+++ vue-webgame/6.lotto/LottoGenerator.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>당첨 숫자</div>
+    <div id="result">
+      <lotto-ball v-for="ball in winBalls"></lotto-ball>
+    </div>
+    <div>보너스</div>
+    <lotto-ball v-if="bonus"></lotto-ball>
+    <button v-if="redo">한 번 더</button>
+  </div>
+</template>
+
+<script>
+import LottoBall from "./LottoBall.vue";
+
+export default {
+  components: {LottoBall},
+  data() {
+    return {
+      winBalls: {},
+      bonus: null,
+      redo: false,
+    }
+  },
+  computed: {},
+  methods: {},
+  beforeDestroy() {
+  },
+  watch: {}
+}
+</script>
+
+<style scoped></style>
