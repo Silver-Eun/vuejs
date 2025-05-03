55a2ddb34721f6c8974e7b819f42a8690c28cc6a
Silver-Eun <h22sang@naver.com>
2025-05-03 23:55:37 +0900

    zerocho - 31
    EventBus

---
+++ vue-webgame/7.TicTakToe_EventBus/TrComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <table>
+    <td-component v-for="(cellData, index) in rowData" :cell-data="cellData"
+                  :key="index"
+                  :cell-index="index" :row-index="rowIndex"></td-component>
+  </table>
+</template>
+
+<script>
+
+import TdComponent from "./TdComponent.vue";
+
+export default {
+  components: {
+    TdComponent,
+  },
+  props: {
+    rowData: Array,
+    rowIndex: Number,
+  }
+}
+</script>
